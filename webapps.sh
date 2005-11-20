e9689537c5fd4ded5b4fe0fe1d4c4aa97f2588c7
Elan Ruusamäe <glen@pld-linux.org>
2005-11-20 17:14:12 +0000

    - program managing webapps symlinks
    Changed files:
        webapps.sh -> 1.1

---
+++ webapps.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+webapps=/etc/webapps
+action="$1"
+webserver="$2"
+app="$3"
+
+webapp_register() {
+	ln -sf $webapps/$app/$webserver.conf /etc/$webserver/webapps/$app.conf
+}
+
+webapp_unregister() {
+	rm -f /etc/$webserver/webapps/$app.conf
+}
+
+usage() {
+	cat >&2 <<EOF
+Usage: $0 register webserver webapp
+Usage: $0 unregister webserver webapp
+EOF
+}
+
+case "$action" in
+register)
+	webapp_register "$webserver" "$app"
+	;;
+unregister)
+	webapp_unregister "$webserver" "$app"
+	;;
+*)
+	usage
+	exit 1
+esac
